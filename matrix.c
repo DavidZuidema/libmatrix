8d3511f5ae31d475d5944e5cb9572c7aa8775c11
David Zuidema <dczuidema@gmail.com>
2017-02-20 14:28:15 -0500

    CODE int matrix create initializes all elements to zero

--- matrix.c
+++ matrix.c
@@ -7,5 +7,6 @@ int_matrix_t *int_matrix_create(const int rows, const int columns)
     int_matrix_t *matrix = malloc(sizeof(int_matrix_t));
     matrix->rows = rows;
     matrix->columns = columns;
+    matrix->data = calloc(rows * columns, sizeof(int));
     return matrix;
 }
