64b6a49ef87c4f79a6631e1a4ec3eb7cdda90949
David Zuidema <dczuidema@gmail.com>
2017-02-20 14:19:56 -0500

    CODE int matrix create sets column count correctly

--- matrix.c
+++ matrix.c
@@ -6,5 +6,6 @@ int_matrix_t *int_matrix_create(const int rows, const int columns)
 {
     int_matrix_t *matrix = malloc(sizeof(int_matrix_t));
     matrix->rows = rows;
+    matrix->columns = columns;
     return matrix;
 }
