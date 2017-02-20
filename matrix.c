216fbc5548a921b308897380ade2346e951e03e2
David Zuidema <dczuidema@gmail.com>
2017-02-20 14:11:58 -0500

    CODE int matrix create sets rows correctly

--- matrix.c
+++ matrix.c
@@ -1,6 +1,10 @@
 #include "matrix.h"
 
+#include <stdlib.h>
+
 int_matrix_t *int_matrix_create(const int rows)
 {
-    return 0;
+    int_matrix_t *matrix = malloc(sizeof(int_matrix_t));
+    matrix->rows = rows;
+    return matrix;
 }
